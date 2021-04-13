5cc434c4cef583568ed87f37728a74a7f613b03b
gaeundev <gaeundev@gmail.com>
2021-04-13 14:39:35 +0900

    생활코딩 React - 15.3 key

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 
 class TOC extends Component {
     render() {
+        let lists = [];
+        let data = this.props.data;
+        console.log(data);
+        let i = 0;
+        while (i < data.length) {
+            lists.push(
+                <li key={data[i].id}>
+                    <a href={'/content/' + data[i].id}>{data[i].title}</a>
+                </li>
+            );
+            i++;
+        }
         return (
             <nav>
                 <ul>
