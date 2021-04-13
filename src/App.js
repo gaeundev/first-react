63e9558a2af5691920cd414c4215f6a8a10994be
gaeundev <gaeundev@gmail.com>
2021-04-13 14:00:47 +0900

    생활코딩 React - 15.2 State 사용

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import Subject from './components/Subject';
 import './App.css';
 
 class App extends Component {
+    // component 가 실행될 때 constructor가 제일 먼저 실행되면서 초기화를 담당한다.
+    constructor(props) {
+        super(props);
+        this.state = {
+            subject: { title: 'WEB', sub: 'World Wide Web!' },
+        };
+    }
     render() {
         return (
             <div className="App">
-                <Subject title="WEB" sub="world wide web!"></Subject>
+                <Subject
+                    title={this.state.subject.title}
+                    sub={this.state.subject.sub}
+                ></Subject>
                 <Subject title="React" sub="For UI"></Subject>
                 <TOC></TOC>
                 <Content
