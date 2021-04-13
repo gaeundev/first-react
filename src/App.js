0676b56d83bf39e2f9f8982abc5faf511b1a6a64
gaeundev <gaeundev@gmail.com>
2021-04-13 11:49:10 +0900

    생활코딩 React - 11. 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// public/pure.html 로 작성된 파일을 컴포넌트로 만드는 과정
+// 컴포넌트를 바라보는 첫번째 시각으로는 '정리정돈'으로 바라보면 좋겠다. - 이고잉
+
+class Subject extends Component {
+    render() {
+        return (
+            <header>
+                <h1>WEB</h1>
+                world wide web!
+            </header>
+        );
+    }
+}
+
+class TOC extends Component {
+    render() {
+        return (
+            <nav>
+                <ul>
+                    <li>
+                        <a href="1.html">HTML</a>
+                    </li>
+                    <li>
+                        <a href="2.css">CSS</a>
+                    </li>
+                    <li>
+                        <a href="3.javascript">JavaScript</a>
+                    </li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+class Content extends Component {
+    render() {
+        return (
+            <article>
+                <h2>HTML</h2>
+                HTML is HyperText Markup Language.
+            </article>
+        );
+    }
+}
+
 class App extends Component {
     render() {
-        return <div className="App">Hello, React!!</div>;
+        return (
+            <div className="App">
+                <Subject></Subject>
+                <TOC></TOC>
+                <Content></Content>
+            </div>
+        );
     }
 }
 
