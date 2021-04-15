625d70eb8cd95bd9e69a2bc2abbd182d1d28976f
gaeundev <gaeundev@gmail.com>
2021-04-15 10:50:56 +0900

    생활코딩 React -16.2 이벤트 설치

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import TOC from './components/TOC';
 import Content from './components/Content';
-import Subject from './components/Subject';
+// import Subject from './components/Subject';
 import './App.css';
 
 class App extends Component {
@@ -35,10 +35,27 @@ class App extends Component {
         }
         return (
             <div className="App">
-                <Subject
+                {/* <Subject
                     title={this.state.subject.title}
                     sub={this.state.subject.sub}
-                ></Subject>
+                ></Subject> */}
+                <header>
+                    <h1>
+                        <a
+                            href="/"
+                            onClick={function (e) {
+                                console.log(e);
+
+                                // e가 포함된 태그의 기본적인 동작을 막는 함수.
+                                // 여기서는 href 가 동작하지 않게된다.
+                                e.preventDefault();
+                            }}
+                        >
+                            {this.state.subject.title}
+                        </a>
+                    </h1>
+                    {this.state.subject.sub}
+                </header>
                 <TOC data={this.state.contents}></TOC>
                 <Content title={_title} desc={_desc}></Content>
             </div>
