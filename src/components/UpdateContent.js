9b8cc197890b890f3d6f131b177e345de49a2669
gaeundev <gaeundev@gmail.com>
2021-04-19 11:54:19 +0900

    생활코딩 React - 20.2 update 구현 : form

--- src/components/UpdateContent.js
+++ src/components/UpdateContent.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 
 class UpdateContent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: this.props.data.title,
+            desc: this.props.data.desc,
+        };
+        this.inputFormHandler = this.inputFormHandler.bind(this);
+    }
+    inputFormHandler(e) {
+        // [] 대괄호 문법은 js 의 새로운 문법
+        // e.target.name 은 태그의 name 값을 가지고 오는 것
+        this.setState({ [e.target.name]: e.target.value });
+    }
     render() {
         console.log('UpdateContent Render');
         console.log(this.props.data);
@@ -24,12 +37,22 @@ class UpdateContent extends Component {
                             type="text"
                             name="title"
                             placeholder="title"
+                            // Warning: You provided a `value` prop to a form field without an `onChange` handler.
+                            // This will render a read-only field.
+                            // If the field should be mutable use `defaultValue`.
+                            // Otherwise, set either `onChange` or `readOnly`.
+                            // value 옵션에 this.props.data.title 을 때려박으면 읽기모드가 되어 수정할 수 없게 된다.
+                            // value={this.props.data.title}
+                            value={this.state.title}
+                            onChange={this.inputFormHandler}
                         ></input>
                     </p>
                     <p>
                         <textarea
                             name="desc"
                             placeholder="description"
+                            value={this.state.desc}
+                            onChange={this.inputFormHandler}
                         ></textarea>
                     </p>
                     <p>
